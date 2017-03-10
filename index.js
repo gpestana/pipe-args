3c73c53aff3da8f78905c693a0c2848b7fb35c04
Pestana Gonçalo <g6pestana@gmail.com>
2017-03-11 00:16:50 +0200

    (feat): Fixes com iojs v3 compatibility

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-module.exports = (() => {
+module.exports = (function()  {
   if(process.stdin.isTTY) return;
   const stdin = require('fs').readFileSync('/dev/stdin').toString();
   process.argv.push(stdin.trim());
--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 const pipe = require('../index');
 const tap = require('tap');
 
-tap.test('it adds stdin to process.argv', t => {
+tap.test('it adds stdin to process.argv', function(t) {
   t.equal(process.argv.length, 3, 'process.argv with 3 eleentss');
   t.equal(process.argv[2], 'piped_arg', 'process.argv with piped arg');
   t.end();
